4231ed0be9b272889735080b3548252681226a17
DavidAbili <89999138+DavidAbili@users.noreply.github.com>
2022-03-04 09:02:45 -0500

    Update InvalidNotationFormatException.java

--- InvalidNotationFormatException.java
+++ InvalidNotationFormatException.java
@@ -1,5 +1,5 @@
 
-public class InvalidNotationFormatException extends Exception {
+public class InvalidNotationFormatException extends RuntimeException {
 
 public InvalidNotationFormatException() {
 	super("The Notation format is incorrect");
